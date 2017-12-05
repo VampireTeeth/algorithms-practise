b0433e113047f69b55ded1df29376a921c4e2355
Steven liu <steven.liu@macquarie.com>
2017-12-05 14:50:09 +1100

    Better solution

--- src/main/java/leetcode/ShortestSubseqContainsAllLabels.java
+++ src/main/java/leetcode/ShortestSubseqContainsAllLabels.java
@@ -1,41 +1,76 @@
 package leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ShortestSubseqContainsAllLabels {
 
-    private static List<Integer> queueSolution(List<String> availableLabels, List<String> targetLabels) {
+    private static List<Integer> solution(List<String> availableLabels, List<String> targetLabels) {
         Set<String> labels = new HashSet<>(availableLabels);
-        int[] q = new int[targetLabels.size()];
-        int rstart = -1, rend = -1, start = 0, end = 0, idx = 0, len = targetLabels.size();
+        Map<String, Integer> appearTimes = new HashMap<>();
+        int rstart = -1, rend = -1, start = 0, end = 0, idx = 0, len = targetLabels.size(), avLen = availableLabels.size(), currLen = len, maxLen = len;
         String currLabel = null;
-        for (start = 0; start < len; q[start++] = -1);
         for (start = 0, end = 0, idx = 0; idx < len; idx++) {
             currLabel = targetLabels.get(idx);
-            if (labels.contains(currLabel)) {
-                if (q[start] != -1 && targetLabels.get(q[start]).equals(currLabel)) start++;
-                q[end++] = idx;
+            if (!labels.contains(currLabel)) continue;
+            start = idx;
+            end = idx;
+            boolean foundEnd = false, foundStart = false;
+            //Moving end ahead until all labels are found or reached len
+            for (;end < len; end++) {
+                currLabel = targetLabels.get(end);
+                if (labels.contains(currLabel)) {
+                    incAppearTimes(appearTimes, currLabel);
+                }
+                if (appearTimes.size() == avLen) {
+                    foundEnd = true;
+                    break;
+                }
             }
-        }
-        rstart = q[start];
-        for (start = 0; start < len; q[start++] = -1);
-        for (start = 0, end = 0, idx = len - 1; idx >= 0; idx--) {
-            currLabel = targetLabels.get(idx);
-            if (labels.contains(currLabel)) {
-                if (q[start] != -1 && targetLabels.get(q[start]).equals(currLabel)) start++;
-                q[end++] = idx;
+
+            //Moving start ahead until the label appears only once or reached end
+            for (;start < end; start++) {
+                currLabel = targetLabels.get(start);
+                Integer times = appearTimes.get(currLabel);
+                if (times != null && times == 1) {
+                    foundStart = true;
+                    break;
+                }
+                if (labels.contains(currLabel)) {
+                    decAppearTimes(appearTimes, currLabel);
+                }
             }
+
+            if (foundEnd && foundStart) {
+                currLen = end - start;
+                maxLen = maxLen > currLen ? currLen : maxLen;
+                rstart = start;
+                rend = end;
+            }
+            idx = end + 1;
+            appearTimes.clear();
         }
-        rend = q[start];
-        return Arrays.asList(rstart, rend);
+        return rstart == -1 ? Arrays.asList(0) : Arrays.asList(rstart, rend);
+    }
+
+    private static void incAppearTimes(Map<String, Integer> appearTimes, String label) {
+        Integer times = appearTimes.get(label);
+        appearTimes.put(label, times == null ? 1 : times + 1);
+    }
+
+    private static void decAppearTimes(Map<String, Integer> appearTimes, String label) {
+        Integer times = appearTimes.get(label);
+        if (times != null && times > 1) appearTimes.put(label, times - 1);
+        else appearTimes.remove(label);
     }
 
     public static void main(String[] args) {
         List<String> availableLabels = Arrays.asList("rain", "the", "spain");
-        List<String> targetLabels = Arrays.asList("the", "sun", "is", "rain", "but", "the", "in", "spain", "aaa", "bbb", "spain");
-        System.out.println(queueSolution(availableLabels, targetLabels));
+        List<String> targetLabels = Arrays.asList("the", "sun", "is", "rain", "but", "the", "in", "spain", "aaa", "bbb", "spain", "but", "the", "rain", "another");
+        System.out.println(solution(availableLabels, targetLabels));
     }
 }
